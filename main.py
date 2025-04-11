cfb09ae19394098ff28651ee23890b3cc3f62ef8
Ankon Karmokar <127048142+ankonkarmokar@users.noreply.github.com>
2025-04-11 17:21:17 +0600

    main.py

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ def index():
     return render_template('index.html', text_output=text_output)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
 
